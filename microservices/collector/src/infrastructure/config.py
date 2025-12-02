00e5dcc77b31dcdc2605574374de39c6ba572c4d
Duarte <duartydev@gmail.com>
2025-12-02 17:57:19 -0400

    chore(collector): update default coordinates to Boa Vista

--- microservices/collector/src/infrastructure/config.py
+++ microservices/collector/src/infrastructure/config.py
@@ -9,8 +9,8 @@ class Config:
     RABBITMQ_USER = os.getenv("RABBITMQ_USER", "user")
     RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", "password")
     QUEUE_NAME = "weather_data"
-    CITY_LAT = -23.5505
-    CITY_LON = -46.6333
+    CITY_LAT = float(os.getenv("CITY_LAT", 2.8235))
+    CITY_LON = float(os.getenv("CITY_LON", -60.6758))
     OPEN_METEO_URL = os.getenv("OPEN_METEO_URL", "https://api.open-meteo.com/v1/forecast")
     SOLAR_PANEL_TILT = int(os.getenv("SOLAR_PANEL_TILT", 25))
     SOLAR_PANEL_AZIMUTH = int(os.getenv("SOLAR_PANEL_AZIMUTH", 180))
